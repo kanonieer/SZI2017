ba57cd52a952d39c3f5a684541e668e1b508ad19
MrVaath <pat.wylegala@gmail.com>
2017-06-11 20:26:53 +0200

    Neural networks upgrade

--- Neural networks/main.js
+++ Neural networks/main.js
@@ -1,110 +1,103 @@
-$(document).ready(function() {
+const mnist = require('mnist');
+const synaptic = require('synaptic');
 
-var net = new brain.NeuralNetwork();
+const set = mnist.set(40); // Przypisujemy zmiennej set randomowe obrazy, odpowiednio 40 dla treningu
 
-var training_data = [
-            {input: { r: 1.0, g: 1.0, b: 1.0 }, output: { grey: 1 }},
-            {input: { r: 1.0, g: 0.9, b: 0.9 }, output: { grey: 1 }},
-            {input: { r: 0.9, g: 0.9, b: 0.9 }, output: { grey: 1 }},
-            {input: { r: 0.9, g: 1.0, b: 1.0 }, output: { grey: 1 }},
-            {input: { r: 0.9, g: 1.0, b: 0.9 }, output: { grey: 1 }},
-            {input: { r: 0.7, g: 0.8, b: 0.8 }, output: { grey: 1 }},
-            {input: { r: 0.6, g: 0.7, b: 0.7 }, output: { grey: 1 }},
-            {input: { r: 0.8, g: 0.8, b: 0.8 }, output: { grey: 1 }},
-            // {input: { r: 0.9, g: 0.9, b: 0.8 }, output: { grey: 1 }},
-            // {input: { r: 0.9, g: 0.9, b: 0.7 }, output: { grey: 1 }},
-            {input: { r: 0.0, g: 1.0, b: 0.0 }, output: { green: 1 }},
-            {input: { r: 0.6, g: 1.0, b: 0.1 }, output: { green: 1 }},
-            {input: { r: 0.6, g: 0.7, b: 0.5 }, output: { green: 1 }},
-            {input: { r: 0.7, g: 0.9, b: 0.2 }, output: { green: 1 }},
-            {input: { r: 0.3, g: 0.8, b: 0.1 }, output: { green: 1 }},
-            {input: { r: 0.2, g: 0.4, b: 0.1 }, output: { green: 1 }},
-            {input: { r: 0.8, g: 1.0, b: 0.6 }, output: { green: 1 }},
-            {input: { r: 0.0, g: 0.6, b: 0.1 }, output: { green: 1 }},
-            // {input: { r: 0.7, g: 1.0, b: 0.4 }, output: { green: 1 }},
-            // {input: { r: 0.9, g: 1.0, b: 0.8 }, output: { green: 1 }},
-            {input: { r: 0.9, g: 0.8, b: 0.5 }, output: { brown: 1 }},
-            {input: { r: 0.8, g: 0.7, b: 0.5 }, output: { brown: 1 }},
-            {input: { r: 0.8, g: 0.5, b: 0.3 }, output: { brown: 1 }},
-            {input: { r: 0.8, g: 0.8, b: 0.6 }, output: { brown: 1 }},
-            {input: { r: 0.8, g: 0.4, b: 0.1 }, output: { brown: 1 }},
-            {input: { r: 0.7, g: 0.5, b: 0.0 }, output: { brown: 1 }},
-            {input: { r: 0.6, g: 0.5, b: 0.0 }, output: { brown: 1 }},
-            {input: { r: 0.5, g: 0.5, b: 0.0 }, output: { brown: 1 }},
-            // {input: { r: 0.7, g: 0.6, b: 0.3 }, output: { brown: 1 }},
-            // {input: { r: 0.6, g: 0.3, b: 0.0 }, output: { brown: 1 }},
-            {input: { r: 0.0, g: 0.0, b: 0.0 }, output: { black: 1 }},
-            {input: { r: 0.0, g: 0.0, b: 0.1 }, output: { black: 1 }},
-            {input: { r: 0.0, g: 0.1, b: 0.0 }, output: { black: 1 }},
-            {input: { r: 0.1, g: 0.1, b: 0.0 }, output: { black: 1 }},
-            {input: { r: 0.2, g: 0.0, b: 0.0 }, output: { black: 1 }},
-            {input: { r: 0.0, g: 0.2, b: 0.1 }, output: { black: 1 }},
-            {input: { r: 0.1, g: 0.1, b: 0.1 }, output: { black: 1 }},
-            {input: { r: 0.1, g: 0.0, b: 0.3 }, output: { black: 1 }},
-            // {input: { r: 0.0, g: 0.0, b: 0.2 }, output: { black: 1 }},
-            // {input: { r: 0.1, g: 0.0, b: 0.1 }, output: { black: 1 }},
-            {input: { r: 1.0, g: 0.0, b: 0.0 }, output: { red: 1 }},
-            {input: { r: 0.9, g: 0.2, b: 0.2 }, output: { red: 1 }},
-            {input: { r: 0.8, g: 0.0, b: 0.0 }, output: { red: 1 }},
-            {input: { r: 0.7, g: 0.1, b: 0.2 }, output: { red: 1 }},
-            {input: { r: 0.8, g: 0.1, b: 0.0 }, output: { red: 1 }},
-            {input: { r: 0.9, g: 0.0, b: 0.9 }, output: { red: 1 }},
-            {input: { r: 0.7, g: 0.1, b: 0.0 }, output: { red: 1 }},
-            {input: { r: 0.8, g: 0.2, b: 0.0 }, output: { red: 1 }},
-            // {input: { r: 0.9, g: 0.2, b: 0.1 }, output: { red: 1 }},
-            // {input: { r: 1.0, g: 0.0, b: 0.2 }, output: { red: 1 }},
-            {input: { r: 0.0, g: 0.0, b: 1.0 }, output: { blue: 1 }},
-            {input: { r: 0.3, g: 0.4, b: 1.0 }, output: { blue: 1 }},
-            {input: { r: 0.2, g: 0.1, b: 0.9 }, output: { blue: 1 }},
-            {input: { r: 0.1, g: 0.4, b: 1.0 }, output: { blue: 1 }},
-            {input: { r: 0.0, g: 0.1, b: 0.6 }, output: { blue: 1 }},
-            {input: { r: 0.1, g: 0.0, b: 0.5 }, output: { blue: 1 }},
-            {input: { r: 0.1, g: 0.1, b: 0.7 }, output: { blue: 1 }},
-            // {input: { r: 0.1, g: 0.1, b: 0.7 }, output: { blue: 1 }},
-            // {input: { r: 0.6, g: 0.8, b: 1.0 }, output: { blue: 1 }},
-            {input: { r: 0.4, g: 0.1, b: 1.0 }, output: { blue: 1 }}];
+const trainingSet = set.training;
+const testSet = mnist[0].set(1, 10); // Ustawia rangę, od 1 do 50, czyli 10 obrazków dla testu
 
-net.train(training_data, {
-  errorThresh: 0.005,  // próg błędu do osiągnięcia
-  iterations: 20000,   // maksymalna liczba iteracji treningu
-  log: true,           // console.log() pokazany progres postępu
-  logPeriod: 10,       // liczba iteracji pomiędzy console.log()
-  learningRate: 0.3    // stopień uczenia się
-})
+// create the layers 
+const Layer = synaptic.Layer;
+const Network = synaptic.Network;
+const Trainer = synaptic.Trainer;
 
-var run1 = net.toFunction();
-var run2 = net.toFunction();
-var run3 = net.toFunction();
-var run4 = net.toFunction();
-var run5 = net.toFunction();
-var run6 = net.toFunction();
-var run7 = net.toFunction();
-var run8 = net.toFunction();
-var run9 = net.toFunction();
-var run10 = net.toFunction();
+const inputLayer = new Layer(2500); // Nasz input -> 2500 = 50 * 50 (nasze obrazy są rozmiaru 50x50)
+const hiddenLayer = new Layer(40);
+const outputLayer = new Layer(10); // 10 ponieważ mamy 10 wyjść, od 0 do 9. Gdzie 0 to bomby, a reszta nie
 
-var output1 = run1({ r: 0.6, g: 0.8, b: 0.3 });
-var output2 = run2({ r: 0.4, g: 0.5, b: 0.1 });
-var output3 = run3({ r: 0.2, g: 1.0, b: 0.1 });
-var output4 = run4({ r: 0.2, g: 0.4, b: 0.1 });
-var output5 = run5({ r: 0.1, g: 1.0, b: 0.6 });
-var output6 = run6({ r: 0.0, g: 1.0, b: 0.0 });
-var output7 = run7({ r: 0.5, g: 0.9, b: 0.1 });
-var output8 = run8({ r: 0.6, g: 0.9, b: 0.0 });
-var output9 = run9({ r: 0.7, g: 0.8, b: 0.5 });
-var output10 = run10({ r: 0.8, g: 1.0, b: 0.4 });
+// connect the layers 
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
 
-console.log(run1.toString());
-console.log(net);
-console.log(output1);
-console.log(output2);
-console.log(output3);
-console.log(output4);
-console.log(output5);
-console.log(output6);
-console.log(output7);
-console.log(output8);
-console.log(output9);
-console.log(output10);
-
+const myNetwork = new Network({
+    
+    // set the layers 
+    input: inputLayer,
+    hidden: [hiddenLayer],
+    output: outputLayer
 });
+
+const trainer = new Trainer(myNetwork);
+trainer.train(trainingSet, {
+    rate: .1, // szybkość uczenia się
+    iterations: 100, // ilość iteracji
+    error: .1, // najniższy prób błędu
+    shuffle: true, // czy nasz zestaw treningowy ma być wybierany losowo
+    log: 1, // wyświetlanie po kolei iteracji w console
+    cost: Trainer.cost.CROSS_ENTROPY
+});
+
+console.log("------------------------------------");
+
+      var output = myNetwork.activate(testSet[0].input);
+      console.log(output);
+      console.log("------------------------------------");
+
+      var maximum = output.reduce(function(p,c) { return p > c ? p : c; });
+      var nominators = output.map(function(e) { return Math.exp(e - maximum); });
+      var denominator = nominators.reduce(function (p, c) { return p + c; });
+      var softmax = nominators.map(function(e) { return e / denominator; });
+
+      var maxIndex = 0;
+      softmax.reduce(function(p, c, i){if (p < c) {maxIndex = i; return c;} else return p;});
+
+      var result = [];
+
+      for (var i = 0; i < output.length; i++)
+      {
+          if (i == maxIndex)
+              result.push(1);
+          else
+              result.push(0);
+      }
+
+      console.log(result);
+      console.log("------------------------------------");
+      console.log("testSet[0].output");
+      console.log("------------------------------------");
+      console.log(testSet[0].output);
+      console.log("------------------------------------");
+
+      var nnDigit = 0;
+
+      for (var i = 0; i <= 9; i++)
+      {
+          if (result[i] == 1)
+          {
+              nnDigit = i;
+              break;
+          }
+      }
+
+      var testDigit = 0;
+
+      for (var i = 0; i <= 9; i++)
+      {
+          if (testSet[0].output[i] == 1)
+          {
+              testDigit = i;
+              break;
+          }
+      }
+
+      if(nnDigit > 0) {
+        console.log("Results:");
+        console.log("------------------------------------");
+        console.log("The neural network believed it wasn't a bomb");
+        console.log("------------------------------------");
+      } else {
+        console.log("Results:");
+        console.log("------------------------------------");
+        console.log("The neural network believed it was a bomb");
+        console.log("------------------------------------");
+      }
--- generowanie planszy/main.js
+++ generowanie planszy/main.js
@@ -1,4 +1,6 @@
 var DecisionTree = require('decision-tree');
+const mnist = require('mnist');
+const synaptic = require('synaptic');
 
  var training_data = [
 {'material':'Metal','weight':3,'shape':'Round','color':'Brown','quality':'New','size':1,'temperature':2,'behavior':'Disarm'},
@@ -226,6 +228,34 @@ var DecisionTree = require('decision-tree');
   var features = ['material','weight','shape','color','quality','size','temperature'];
 
   var dt = new DecisionTree(training_data, class_name, features);
+
+  const set = mnist.set(20); // Przypisujemy zmiennej set randomowe obrazy, odpowiednio 20 dla treningu
+
+  const trainingSet = set.training;
+  const testSet = mnist[0].set(1, 5); // Ustawia rangę, od 1 do 5, czyli 5 obrazków dla testu
+
+  // create the layers 
+  const Layer = synaptic.Layer;
+  const Network = synaptic.Network;
+  const Trainer = synaptic.Trainer;
+
+  const inputLayer = new Layer(2500); // Nasz input -> 2500 = 50 * 50 (nasze obrazy są rozmiaru 50x50)
+  const hiddenLayer = new Layer(20);
+  const outputLayer = new Layer(10); // 10 ponieważ mamy 10 wyjść, od 0 do 9. Gdzie 0 to bomby, a reszta nie
+
+  // connect the layers 
+  inputLayer.project(hiddenLayer);
+  hiddenLayer.project(outputLayer);
+
+  const myNetwork = new Network({
+      
+      // set the layers 
+      input: inputLayer,
+      hidden: [hiddenLayer],
+      output: outputLayer
+  });
+
+  const trainer = new Trainer(myNetwork);
 
 $(document).ready(function() {
   var testModel;
@@ -592,6 +622,79 @@ $(document).ready(function() {
         'temperature':bombArrayForTree[index].bombObject.temperature
       }
       console.log('Predykcja zachowania: '+ dt.predict(bomb));
+      trainer.train(trainingSet, {
+        rate: .2, // szybkość uczenia się
+        iterations: 50, // ilość iteracji
+        error: .1, // najniższy prób błędu
+        shuffle: true, // czy nasz zestaw treningowy ma być wybierany losowo
+        log: 1, // wyświetlanie po kolei iteracji w console
+        cost: Trainer.cost.CROSS_ENTROPY
+      });
+
+      console.log("------------------------------------");
+
+      var output = myNetwork.activate(testSet[0].input);
+      console.log(output);
+      console.log("------------------------------------");
+
+      var maximum = output.reduce(function(p,c) { return p > c ? p : c; });
+      var nominators = output.map(function(e) { return Math.exp(e - maximum); });
+      var denominator = nominators.reduce(function (p, c) { return p + c; });
+      var softmax = nominators.map(function(e) { return e / denominator; });
+
+      var maxIndex = 0;
+      softmax.reduce(function(p, c, i){if (p < c) {maxIndex = i; return c;} else return p;});
+
+      var result = [];
+
+      for (var i = 0; i < output.length; i++)
+      {
+          if (i == maxIndex)
+              result.push(1);
+          else
+              result.push(0);
+      }
+
+      console.log(result);
+      console.log("------------------------------------");
+      console.log("testSet[0].output");
+      console.log("------------------------------------");
+      console.log(testSet[0].output);
+      console.log("------------------------------------");
+
+      var nnDigit = 0;
+
+      for (var i = 0; i <= 9; i++)
+      {
+          if (result[i] == 1)
+          {
+              nnDigit = i;
+              break;
+          }
+      }
+
+      var testDigit = 0;
+
+      for (var i = 0; i <= 9; i++)
+      {
+          if (testSet[0].output[i] == 1)
+          {
+              testDigit = i;
+              break;
+          }
+      }
+
+      if(nnDigit > 0) {
+        console.log("Results:");
+        console.log("------------------------------------");
+        console.log("The neural network believed it wasn't a bomb");
+        console.log("------------------------------------");
+      } else {
+        console.log("Results:");
+        console.log("------------------------------------");
+        console.log("The neural network believed it was a bomb");
+        console.log("------------------------------------");
+      }
     }
     grid[actualX][actualY]='Valid';
     //console.log(grid[bomb[index][0]][bomb[index][1]] );
